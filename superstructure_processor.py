28b5617b0d0d304cc0af25314ca1181a925ae598
Jaika Diya <jdiya@naf-cms17.desy.de>
2026-07-27 14:38:27 +0200

    plots for reco jet

--- superstructure_processor.py
+++ superstructure_processor.py
@@ -21,6 +21,10 @@ class Processor(pepper.ProcessorBasicPhysics):
         selector.set_multiple_columns(self.find_genjets_matching_HP)
         self.unload_column("GenJet")
         selector.add_cut("Require_matched_genjets", self.require_distinct_genjets)
+        selector.set_column("Jet", self.calculate_jet_pull)
+        selector.set_multiple_columns(self.find_jets_matching_HP)
+        self.unload_column("Jet")
+        selector.add_cut("Require_matched_jets", self.require_distinct_jets)
         selector.set_multiple_columns(self.set_pull_angles)
 
     def find_progenitor_quarks(self, data):
@@ -60,6 +64,21 @@ class Processor(pepper.ProcessorBasicPhysics):
         jets = jets[ak.num(cands, axis=2) > 0]
         return jets
 
+    def calculate_jet_pull(self, data):
+        def weighted_sum(deltas, pt_cands, pt_jet, mag):
+            return ak.sum(deltas * pt_cands * mag / pt_jet, axis=2)
+
+        jets = data["Jet"]
+        #cands = jets.candidates
+        cands = jets.constituents.pf
+        mag = cands.deltaRapidityPhi(jets)
+        dphis = cands.deltaphi(jets)
+        dy = cands.rapidity - jets.rapidity
+        jets["pull_phi"] = weighted_sum(dphis, cands.pt, jets.pt, mag)
+        jets["pull_rapidity"] = weighted_sum(dy, cands.pt, jets.pt, mag)
+        jets = jets[ak.num(cands, axis=2) > 0]
+        return jets
+
     def find_genjets_matching_HP(self, data):
         new_cols = {}
         gen_jets = data["GenJet"]
@@ -68,6 +87,16 @@ class Processor(pepper.ProcessorBasicPhysics):
         new_cols["genjet_from_HP_bbar"] = ak.drop_none(data["gen_HP_bbar"].nearest(gen_jets, threshold=0.3))
         new_cols["genjet_from_HP_qfromWplus"] = ak.drop_none(data["gen_HP_qfromWplus"].nearest(gen_jets, threshold=0.3))
         new_cols["genjet_from_HP_qfromWminus"] = ak.drop_none(data["gen_HP_qfromWminus"].nearest(gen_jets, threshold=0.3))
+        return new_cols
+
+    def find_jets_matching_HP(self, data):
+        new_cols = {}
+        jets = data["Jet"]
+        jets = ak.with_field(jets, ak.local_index(jets, axis=1), "idx")
+        new_cols["jet_from_HP_b"] = ak.drop_none(data["gen_HP_b"].nearest(jets, threshold=0.3))
+        new_cols["jet_from_HP_bbar"] = ak.drop_none(data["gen_HP_bbar"].nearest(jets, threshold=0.3))
+        new_cols["jet_from_HP_qfromWplus"] = ak.drop_none(data["gen_HP_qfromWplus"].nearest(jets, threshold=0.3))
+        new_cols["jet_from_HP_qfromWminus"] = ak.drop_none(data["gen_HP_qfromWminus"].nearest(jets, threshold=0.3))
         return new_cols
 
     def require_distinct_genjets(self, data):
@@ -79,6 +108,17 @@ class Processor(pepper.ProcessorBasicPhysics):
                 (ak.num(data["genjet_from_HP_bbar"]) == 1) &
                 (ak.num(data["genjet_from_HP_qfromWplus"]) == 2) &
                 (ak.num(data["genjet_from_HP_qfromWminus"]) == 2) &
+                all_distinct)
+
+    def require_distinct_jets(self, data):
+        idx_all = ak.concatenate(
+            [j.idx for j in [data["jet_from_HP_b"], data["jet_from_HP_bbar"], data["jet_from_HP_qfromWplus"], data["jet_from_HP_qfromWminus"]]], axis=1)
+        idx_sorted = ak.sort(idx_all, axis=1)
+        all_distinct = ak.all(idx_sorted[:, 1:] != idx_sorted[:, :-1], axis=1)
+        return ((ak.num(data["jet_from_HP_b"]) == 1) &
+                (ak.num(data["jet_from_HP_bbar"]) == 1) &
+                (ak.num(data["jet_from_HP_qfromWplus"]) == 2) &
+                (ak.num(data["jet_from_HP_qfromWminus"]) == 2) &
                 all_distinct)
 
     @staticmethod
@@ -103,5 +143,12 @@ class Processor(pepper.ProcessorBasicPhysics):
             self.calculate_pull_angle(data["genjet_from_HP_qfromWminus"])
         new_cols["forward_pull_angle_bs"], new_cols["backward_pull_angle_bs"] = \
             self.calculate_pull_angle(ak.concatenate([data["genjet_from_HP_b"], data["genjet_from_HP_bbar"]], axis=1))
+        new_cols["reco_forward_pull_angle_Wplus"], new_cols["reco_backward_pull_angle_Wplus"] = \
+            self.calculate_pull_angle(data["jet_from_HP_qfromWplus"])
+        new_cols["reco_forward_pull_angle_Wminus"], new_cols["reco_backward_pull_angle_Wminus"] = \
+            self.calculate_pull_angle(data["jet_from_HP_qfromWminus"])
+        new_cols["reco_forward_pull_angle_bs"], new_cols["reco_backward_pull_angle_bs"] = \
+            self.calculate_pull_angle(ak.concatenate([data["jet_from_HP_b"], data["jet_from_HP_bbar"]], axis=1))
         return new_cols
+
 
